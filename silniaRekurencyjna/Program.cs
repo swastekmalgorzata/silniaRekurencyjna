c49424224437729cd0abbf808c51fd0b2a7b32a5
swastek_malgorzata <swastekm@gmail.com>
2022-04-30 22:11:59 +0200

    Add project files.

---
+++ silniaRekurencyjna/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace silniaRekurencyjna
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int x = 4;
+            Console.WriteLine("silnia dla " + x + " to");
+            Console.WriteLine(wyliczSilnie(x)); 
+            Console.ReadKey();
+        }
+            static int wyliczSilnie(int n)
+            {
+                if (n <= 1) return 1;
+                else return n * wyliczSilnie(n - 1);
+                
+            }
+        
+    }
+}
